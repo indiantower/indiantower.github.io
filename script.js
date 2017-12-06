75aaee977e20896777be5dcd7184ae05965f8a2d
Travis <tjxhahn@gmail.com>
2017-12-06 13:23:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 alert('Testing Site Script');
+function getWinner() {
+  let userOne = prompt('What is your move? User One?').toLowerCase();
+  let userTwo = prompt('What is your move? User Two?').toLowerCase();
+   if (userOne === 'rock' && userTwo === 'paper') {
+     document.write('User Two Wins!');
+   }
+   else if (userOne === "scissors" && userTwo === 'rock') {
+     document.write('User Two Wins!');
+   }
+   else if (userOne === 'paper' && userTwo === "scissors") {
+     document.write('User Two Wins!');
+   }
+   if (userOne === 'rock' && userTwo === 'scissors') {
+     document.write('User One Wins!');
+   }
+   else if (userOne === "scissors" && userTwo === 'paper') {
+     document.write('User One Wins!');
+   }
+   else if (userOne === 'paper' && userTwo === "rock") {
+     document.write('User One Wins!');
+   }
+   else if (userOne === 'rock' && userTwo === 'rock' || userOne === 'paper' && userTwo === 'paper' || userOne === 'scissors' && userTwo === 'scissors') {
+     document.write('Tie!');
+   }
+   return 0;
+}
